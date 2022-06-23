55fd2ffe4c0bebb3c0f154269d7b6dec5dbdc0ab
Tan Tran <tantt.dev@gmail.com>
2022-06-22 22:21:50 -0400

    Add card transition animation

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     let cols = [GridItem(.adaptive(minimum: 85))]
     @StateObject var viewModel: EmojiMemoryGame
+    @State private var dealt = Set<Int>()
     
     var body: some View {
         VStack {
@@ -27,22 +28,38 @@ struct ContentView: View {
         AspectVGrid(items: viewModel.cards, aspectRatio: 2/3) { card in
             cardView(for: card)
         }
+        .onAppear(perform: {
+            withAnimation {
+                for card in viewModel.cards {
+                    dealt(card)
+                }
+            }
+        })
         .foregroundColor(.red )
     }
     
     @ViewBuilder
     private func cardView(for card: EmojiMemoryGame.Card) -> some View {
-        if card.isMatched && !card.isFaceUp {
+        if unDealt(card) || (card.isMatched && !card.isFaceUp) {
             Rectangle().opacity(0)
         } else {
             CardView(card: card)
                 .padding(4)
+                .transition(AnyTransition.asymmetric(insertion: .scale, removal: .opacity))
                 .onTapGesture {
                     withAnimation {
                         viewModel.choose(card)
                     }
                 }
         }
+    }
+    
+    private func dealt(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func unDealt(_ card: EmojiMemoryGame.Card) -> Bool {
+        !dealt.contains(card.id)
     }
 }
 
